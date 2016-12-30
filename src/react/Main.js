2827d2d8ced8d76ff840deeea7b2743729f15181
Jeroen <jeroen.berrevoets@gmail.com>
2016-12-30 23:56:32 +0100

    Add Title component architecture
    INFO: only to illustrate we can devide this website in react ccomponents -> will be default in the future

--- src/react/Main.js
+++ src/react/Main.js
@@ -1,19 +1,21 @@
 import React from 'react'
 
 import { getRCJson } from '../helpers'
+import Title from './Title'
 
 export default class Main extends React.Component {
   render() {
+    const params = getRCJson()
+
     return (
       <html lang="en">
         <head>
           <meta charSet="utf-8" />
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
-          <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/latest/css/bootstrap.min.css" />
-          <title>{getRCJson().title}</title>
+          <meta name="viewport" content="width=device-width, initial-scale=1" />          
+          <title>{params.title}</title>
         </head>
         <body>
-          <div>Hello, World!</div>
+          <Title>{params.title}</Title>
         </body>
       </html>
     )
