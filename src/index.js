68da2a0ec2593a7e5eb5798f9ce1a5bbd697f512
Jeroen <jeroen.berrevoets@gmail.com>
2016-12-30 22:16:20 +0100

    read .gwrc file as a test

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
-module.exports = () => {
+import fs from 'fs'
+
+module.exports = {
+  test: function() {
   let dir = require.main.filename.split('/')
   dir[dir.length - 1] = '.gwrc'
+  dir = dir.join('/')
 
-  console.log(dir.join('/'))
+  console.log(JSON.parse(fs.readFileSync(dir)))
+}
 }
