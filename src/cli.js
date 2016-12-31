3dafa5e6fdfcb62b096aa2e9cab426c76bf978bf
Jeroen <jeroen.berrevoets@gmail.com>
2016-12-31 13:01:14 +0100

    create some subcommands

--- src/cli.js
+++ src/cli.js
@@ -1,2 +1,10 @@
 #! /usr/bin/env node
-console.log('the CLI tool works!')
+import yargs from 'yargs'
+
+const argv = yargs.usage("$0 command")
+  .command("init", "create a new generic website")
+  .command("change", "update any field with a new value")
+  .demand(1, "must provide a valid command")
+  .help("h")
+  .alias("h", "help")
+  .argv
