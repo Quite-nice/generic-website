c72516514946f3e11981de1244d0a292200a4690
Jeroen <jeroen.berrevoets@gmail.com>
2017-01-01 21:16:26 +0100

    Use inquirer package to create question chain

--- src/cli/cmds/init.js
+++ src/cli/cmds/init.js
@@ -1,3 +1,18 @@
+import { touch } from 'shelljs'
+import inquirer from 'inquirer'
+
+const chain = [
+  {
+    type: 'input', name: 'title', message: 'title: ', validate: (val) => val.constructor === String?true: 'please provide a valid title'
+  }
+]
+
 export const init = () => {
-  console.log('init from init file')
+  // CREATE SUMMARY FILE
+  touch('.generic.json')
+  console.log(require('./statics').explain.init)
+
+  // FILL OUT THE SUMMARY FILE
+  inquirer.prompt(chain).then((res) => console.log(JSON.stringify(res)))
+
 }
