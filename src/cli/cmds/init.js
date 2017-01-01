7e22f9598b35efec9c1653b4dc1b819d8d41655d
Jeroen <jeroen.berrevoets@gmail.com>
2017-01-01 21:51:38 +0100

    Create first draft of standard questions

--- src/cli/cmds/init.js
+++ src/cli/cmds/init.js
@@ -1,18 +1,23 @@
-import { touch } from 'shelljs'
+import { touch, echo, to } from 'shelljs'
 import inquirer from 'inquirer'
 
+const typeValidator = (val, key, type) => val.constructor === type? true: `please provide a valid ${key}`
+
 const chain = [
-  {
-    type: 'input', name: 'title', message: 'title: ', validate: (val) => val.constructor === String?true: 'please provide a valid title'
-  }
+  {type: 'input', name: 'title', message: 'title: ', validator: (val) => typeValidator(val, 'title', String)},
+  {type: 'input', name: 'subtitle', message: 'subtitle: ', validator: (val) => typeValidator(val, 'subtitle', String)},
+  {type: 'input', name: 'description', message: 'description: ', validator: (val) => typeValidator(val, 'message', String)},
+  {type: 'input', name: 'mail', message: 'mail: ', validator: (val) => typeValidator(val, 'mail', String)},
+  {type: 'input', name: 'github', message: 'github username: ', validator: (val) => typeValidator(val, 'github username', String)},
+  {type: 'input', name: 'facebook', message: 'facebook username: ', validator: (val) => typeValidator(val, 'facebook username', String)},
+  {type: 'input', name: 'twitter', message: 'twitter handler (no @ required): ', validator: (val) => typeValidator(val, 'twitter handler', String)}
 ]
 
 export const init = () => {
   // CREATE SUMMARY FILE
   touch('.generic.json')
-  console.log(require('./statics').explain.init)
+  console.log(require('./static-texts').explain.init)
 
   // FILL OUT THE SUMMARY FILE
-  inquirer.prompt(chain).then((res) => console.log(JSON.stringify(res)))
-
+  inquirer.prompt(chain).then((res) => echo(JSON.stringify(res)).to('./.generic.json'))
 }
