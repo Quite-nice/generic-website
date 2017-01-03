74e513003fd40463b5c771d0a8cd08718de86eb2
Jeroen <jeroen.berrevoets@gmail.com>
2017-01-03 23:26:13 +0100

    implement flat field add in cli

--- src/cli/cmds/add.js
+++ src/cli/cmds/add.js
@@ -1,3 +1,13 @@
-export const add = () => {
-  console.log('add from file')
+import inquirer from 'inquirer'
+
+import { typeValidator, readJSON, writeJSON } from '../../helpers'
+
+export const add = (field) => {
+  const generic = readJSON('./.generic.json')
+
+  inquirer.prompt([{type: 'input', name: field, message: `${field}: `,
+  validator: (val) => typeValidator(val, field, String)}]).then((answer) => {
+    generic[field] = answer[field]
+    writeJSON('./.generic.json', generic)
+  })
 }
--- src/helpers.js
+++ src/helpers.js
@@ -1,5 +1,7 @@
 import fs from 'fs'
+import { echo, to } from 'shelljs'
 
 export const readJSON = (location) => JSON.parse(fs.readFileSync(location))
+export const writeJSON = (location, obj) => echo(JSON.stringify(obj, null, 2)).to(location)
 export const getRCJson = () => readJSON(`${__dirname.split('node_modules')[0]}/.gwrc`)
 export const typeValidator = (val, key, type) => val.constructor === type? true: `please provide a valid ${key}`
