8552647a246128a0cc0840d7deffd175d5496dc5
Jeroen <jeroen.berrevoets@gmail.com>
2017-01-03 22:51:10 +0100

    Require <field> in add cmd

--- src/cli/cli.js
+++ src/cli/cli.js
@@ -5,7 +5,7 @@ import { init, change, add, deploy } from './cmds'
 const argv = yargs.usage("$0 command")
   .command("init", "create a new generic website")
   .command("change <field>", "update any field with a new value")
-  .command("add", "add a new key and value")
+  .command("add <field>", "add a new key and value")
   .command("deploy", "deploy your website")
   .demand(1, "must provide a valid command")
   .help("h")
@@ -15,6 +15,6 @@ const argv = yargs.usage("$0 command")
 switch (argv._[0]) {
   case 'init': init(); break
   case 'change': change(argv.field); break
-  case 'add': add(); break
+  case 'add': add(argv.field); break
   case 'deploy': deploy(); break
 }
--- src/cli/cmds/change.js
+++ src/cli/cmds/change.js
@@ -5,7 +5,6 @@ import _ from 'lodash'
 
 import { typeValidator, readJSON } from '../../helpers'
 
-
 export const change = (field) => {
   const generic = readJSON('./.generic.json')
 
@@ -15,14 +14,13 @@ export const change = (field) => {
   })
 }
 
-
 function getQuestions(current, field, path) {
   if (current.constructor === String) {
     return [{type: 'input', name: field, message: `${field}: `, validator: (val) => typeValidator(val, field, String)}]
   } else {
     const strings = Object.keys(current).filter((key) => current[key].constructor === String)
     const objects = _.difference(Object.keys(current), strings)
-    
+
     const questions = [...strings.map((s) => ({type: 'input', name: `${path}${s}`, message: `${path}${s}: `, validator: (val) => typeValidator(val, field, String)}))]
     for (const o of objects) {
       questions.push(...getQuestions(current[o], o, `${path}${o}.`))
