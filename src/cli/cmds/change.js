df7f8c1f2cd815f1b21da4297d6953963a7972c0
Jeroen <jeroen.berrevoets@gmail.com>
2017-01-03 22:44:15 +0100

    Implement recursive change cli

--- src/cli/cmds/change.js
+++ src/cli/cmds/change.js
@@ -1,21 +1,32 @@
 import fs from 'fs'
 import inquirer from 'inquirer'
 import { echo, to } from 'shelljs'
+import _ from 'lodash'
 
 import { typeValidator, readJSON } from '../../helpers'
 
 
 export const change = (field) => {
-  // GET CURRENT GENERIC
   const generic = readJSON('./.generic.json')
 
-  // ASK THE QUESTION
-  const question = [{type: 'input', name: field, message: `${field}: `, validator: (val) => typeValidator(val, field, String)}]
-  inquirer.prompt(question).then((r) => {
-    // CHANGE GENERIC
-    generic[field] = r[field]
-
-    //WRITE IT
-    echo(JSON.stringify(generic)).to('./.generic.json')
+  inquirer.prompt(getQuestions(generic[field], field, '')).then((answer) => {
+    generic[field] = answer
+    echo(JSON.stringify(generic, null, 2)).to('./.generic.json')
   })
+}
+
+
+function getQuestions(current, field, path) {
+  if (current.constructor === String) {
+    return [{type: 'input', name: field, message: `${field}: `, validator: (val) => typeValidator(val, field, String)}]
+  } else {
+    const strings = Object.keys(current).filter((key) => current[key].constructor === String)
+    const objects = _.difference(Object.keys(current), strings)
+    
+    const questions = [...strings.map((s) => ({type: 'input', name: `${path}${s}`, message: `${path}${s}: `, validator: (val) => typeValidator(val, field, String)}))]
+    for (const o of objects) {
+      questions.push(...getQuestions(current[o], o, `${path}${o}.`))
+    }
+    return questions
+  }
 }
