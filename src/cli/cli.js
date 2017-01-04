900ef0236d288e29ff715030f53db78c13afa6c1
Jeroen <jeroen.berrevoets@gmail.com>
2017-01-04 15:55:51 +0100

    Add section options for add cmd

--- src/cli/cli.js
+++ src/cli/cli.js
@@ -5,8 +5,14 @@ import { init, change, add, deploy } from './cmds'
 const argv = yargs.usage("$0 command")
   .command("init", "create a new generic website")
   .command("change <field>", "update any field with a new value")
-  .command("add <field>", "add a new key and value")
-  .command("deploy", "deploy your website")
+  .command("add", "add a new key and value", (yargs) => (
+    yargs
+      .option("s", {alias: "section", describe: "add a section"})
+      .option("f", {alias: "field", describe: "provide a field name"})
+      .demandOption(['f'], "Please provide a field name, it is required.")
+      .help("h").alias("h", "help")
+  ))
+  .command("deploy <location>", "deploy your website")
   .demand(1, "must provide a valid command")
   .help("h")
   .alias("h", "help")
@@ -15,6 +21,6 @@ const argv = yargs.usage("$0 command")
 switch (argv._[0]) {
   case 'init': init(); break
   case 'change': change(argv.field); break
-  case 'add': add(argv.field); break
+  case 'add': console.log(argv); add(argv.field); break
   case 'deploy': deploy(); break
 }
