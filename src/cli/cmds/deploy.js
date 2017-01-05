aa4f07211bfc762b26c41e9662d4d71c0afd1203
Jeroen <jeroen.berrevoets@gmail.com>
2017-01-05 23:15:33 +0100

    check for generic.json on deploy

--- src/cli/cmds/deploy.js
+++ src/cli/cmds/deploy.js
@@ -1,9 +1,23 @@
 import { exec } from 'shelljs'
+import inquirer from 'inquirer'
 
 import server from '../../server'
+import { readJSON } from '../../helpers'
+import { init } from './init'
 
 export let port = 3000
 export const deploy = (args) => {
-  if (parseInt(args.p)) port = parseInt(args.p)
-  server()
+  try {
+    readJSON('./.generic.json')
+
+    if (parseInt(args.p)) port = parseInt(args.p)
+    server()
+  } catch (e) {
+    inquirer.prompt([{type: 'confirm', name: 'initialize', message: `Looks like you haven't yet initialized this project. Would you like to?`}])
+      .then(({initialize}) => {
+        if (initialize) {
+          init()
+        }
+      })
+  }
 }
