d8842f78681fc21c2e43509b50bb9e6327fbb8d4
Jeroen <jeroen.berrevoets@gmail.com>
2017-01-05 22:33:28 +0100

    First Step

--- src/cli/cmds/generate.js
+++ src/cli/cmds/generate.js
@@ -1,3 +1,7 @@
-export const generate = () => {
+import { echo, to } from 'shelljs'
+
+export const generate = (args) => {
   console.log('generate')
+  // 1 copy parameterized express.js file
+  // 2 create generated bundle file of react elements
 }
