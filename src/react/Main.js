e67a3813f46b01955a3fa75fa167fc285a6282cf
Jeroen <jeroen.berrevoets@gmail.com>
2017-01-06 23:26:18 +0100

    Reorganize Main component to handle generic structure

--- src/react/Main.js
+++ src/react/Main.js
@@ -5,16 +5,17 @@ import Title from './Title'
 
 export default class Main extends React.Component {
   render() {
-    const { generic } = this.props
+    const { head } = this.props.generic
+    console.log(this.props.generic)
     return (
       <html lang="en">
         <head>
           <meta charSet="utf-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
-          <title>{generic.title}</title>
+          <title>{head.title}</title>
         </head>
         <body>
-          <Title>{generic.title}</Title>
+          <Title>{head.title}</Title>
         </body>
       </html>
     )
@@ -22,36 +23,38 @@ export default class Main extends React.Component {
 }
 
 Main.propTypes = {
-  // Required
-  head: React.PropTypes.shape({
-    title: React.PropTypes.string.isRequired,
+  generic: React.PropTypes.shape({
+    // Required
+    head: React.PropTypes.shape({
+      title: React.PropTypes.string.isRequired,
 
-    subtitle: React.PropTypes.string,
-    description: React.PropTypes.string,
-  }).isRequired,
+      subtitle: React.PropTypes.string,
+      description: React.PropTypes.string,
+    }).isRequired,
 
-  // Optional
-  constact: React.PropTypes.shape({
-    mail: React.PropTypes.string,
-    facebook: React.PropTypes.string,
-    twitter: React.PropTypes.string,
-    github: React.PropTypes.string,
-  }),
-  style: React.PropTypes.shape({
-    accentColor: React.PropTypes.string,
-    theme: React.PropTypes.string,
-  }),
-  sections: React.PropTypes.arrayOf(React.PropTypes.shape({
-    rank: React.PropTypes.number.isRequired,
+    // Optional
+    constact: React.PropTypes.shape({
+      mail: React.PropTypes.string,
+      facebook: React.PropTypes.string,
+      twitter: React.PropTypes.string,
+      github: React.PropTypes.string,
+    }),
+    style: React.PropTypes.shape({
+      accentColor: React.PropTypes.string,
+      theme: React.PropTypes.string,
+    }),
+    sections: React.PropTypes.arrayOf(React.PropTypes.shape({
+      rank: React.PropTypes.number.isRequired,
 
-    title: React.PropTypes.string,
-    description: React.PropTypes.string,
-    color: React.PropTypes.string,
-  })),
-  plugins: React.PropTypes.arrayOf(React.PropTypes.shape({
-    rank: React.PropTypes.number.isRequired,
-    component: React.PropTypes.string.isRequired,
-  })),
+      title: React.PropTypes.string,
+      description: React.PropTypes.string,
+      color: React.PropTypes.string,
+    })),
+    plugins: React.PropTypes.arrayOf(React.PropTypes.shape({
+      rank: React.PropTypes.number.isRequired,
+      component: React.PropTypes.string.isRequired,
+    })),
+  }).isRequired
 }
 
 Main.defaultProps = {
