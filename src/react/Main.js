4d06f826a5cfb1ca3a54643282ed20214f40586e
Jeroen <jeroen.berrevoets@gmail.com>
2017-01-06 23:04:12 +0100

    Set some PropTypes in main.js following the init guidelines
    WARNING: might be a little out of date, still need to check for certainty

--- src/react/Main.js
+++ src/react/Main.js
@@ -20,3 +20,42 @@ export default class Main extends React.Component {
     )
   }
 }
+
+Main.propTypes = {
+  // Required
+  head: React.PropTypes.shape({
+    title: React.PropTypes.string.isRequired,
+
+    subtitle: React.PropTypes.string,
+    description: React.PropTypes.string,
+  }).isRequired,
+
+  // Optional
+  constact: React.PropTypes.shape({
+    mail: React.PropTypes.string,
+    facebook: React.PropTypes.string,
+    twitter: React.PropTypes.string,
+    github: React.PropTypes.string,
+  }),
+  style: React.PropTypes.shape({
+    accentColor: React.PropTypes.string,
+    theme: React.PropTypes.string,
+  }),
+  sections: React.PropTypes.arrayOf(React.PropTypes.shape({
+    rank: React.PropTypes.number.isRequired,
+
+    title: React.PropTypes.string,
+    description: React.PropTypes.string,
+    color: React.PropTypes.string,
+  })),
+  plugins: React.PropTypes.arrayOf(React.PropTypes.shape({
+    rank: React.PropTypes.number.isRequired,
+    component: React.PropTypes.string.isRequired,
+  })),
+}
+
+Main.defaultProps = {
+  style: {
+    accentColor: 'grey',
+  }
+}
