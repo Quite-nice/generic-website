c5ba85f916325f09a74f70cb1766117dc903b1bb
Jeroen <jeroen.berrevoets@gmail.com>
2017-01-10 20:18:22 +0100

    Initial draft for a Contact Component

--- src/ui/react/Contact.js
+++ src/ui/react/Contact.js
@@ -1,3 +1,38 @@
 import React, { Component } from 'react'
 
-export default () => <p>Contact</p>
+const urlMap = {
+  github: 'https://github.com',
+  facebook: 'https://facebook.com',
+  twitter: 'https://twitter.com',
+  mail: 'mailto:',
+}
+
+export default class Contact extends Component {
+  getContacts() {
+    return Object.keys(this.props.contact).map((c) => <ContactElement account={c} />)
+  }
+
+  render() {
+    return (
+      <ul>
+        {this.getContacts()}
+      </ul>
+    )
+  }
+}
+
+class ContactElement extends Component {
+  render() {
+    return (
+      <div style={{width: '20%'}} >
+        <a href={`${urlMap[c]}${this.prps.contact[c]}`} >
+          {this.props.contact[c]}
+        </a>
+      </div>
+    )
+  }
+}
+
+ContactElement.propTypes = {
+  account: React.PropTypes.string.isRequired
+}
